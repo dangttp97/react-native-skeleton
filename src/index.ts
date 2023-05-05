3d15ff0d8b8032f9313e104026c48ce223610e53
Trần Thành Phương Đăng <phuongdang012@gmail.com>
2023-05-05 10:17:21 +0700

    chore: - export memo component

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
+import React from 'react';
 import { SkeletonWrapper } from './SkeletonWrapper';
 
 export * from './SkeletonFlatList';
-export default SkeletonWrapper;
+export default React.memo(SkeletonWrapper);
